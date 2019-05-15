2ac89ce003b8f04522ba6ce148b3f615e650344f
ivanklimuk <ivanklimuk96@gmail.com>
2019-05-15 21:46:20 +0300

    Adjust the train process and parametersa

--- train.py
+++ train.py
@@ -82,10 +82,12 @@ def train_and_evaluate(model, train_data, val_data, epochs, batch_size, optimize
         restore_file: (string) optional- name of file to restore from (without its extension .pth.tar)
     """
     # reload weights from restore_file if specified
+    '''
     if restore_file is not None:
         restore_path = os.path.join(model_path, args.restore_file + '.pth.tar')
         logging.info("Restoring parameters from {}".format(restore_path))
         utils.load_checkpoint(restore_path, model, optimizer)
+    '''
 
     best_val_acc = 0.0
 
@@ -95,33 +97,33 @@ def train_and_evaluate(model, train_data, val_data, epochs, batch_size, optimize
 
         # Compute number of batches in one epoch (one full pass over the training set)
         num_steps = (len(train_data['data']) + 1) // batch_size
-        train_data_iterator = data_iterator(data=train_data['data'], labels=train_data['labels'], batch_size=BATCH_SIZE, shuffle=True)
+        train_data_iterator = data_iterator(data=train_data['data'], labels=train_data['labels'], batch_size=batch_size, shuffle=True)
         train(model, optimizer, loss_function, train_data_iterator, metrics, num_steps)
-        
+
         # Calculate the train confusion matrix and ROC AUC score
         roc_confusion(model=model, data=train_data, batch_size=batch_size)
-        
+
         # Evaluate for one epoch on validation set
         num_steps = (len(val_data['data']) + 1) // batch_size
-        val_data_iterator = data_iterator(data=val_data['data'], labels=val_data['labels'], batch_size=BATCH_SIZE, shuffle=False)
+        val_data_iterator = data_iterator(data=val_data['data'], labels=val_data['labels'], batch_size=batch_size, shuffle=False)
         val_metrics = evaluate(model, loss_function, val_data_iterator, metrics, num_steps)
-        
+
         # Calculate the validation confusion matrix and ROC AUC score
         roc_confusion(model=model, data=val_data, mode='Val', batch_size=batch_size)
-        
+
         val_acc = val_metrics['f1']
         is_best = val_acc > best_val_acc
 
         # Save weights
         utils.save_checkpoint({'epoch': epoch + 1,
-                               'state_dict' : model.state_dict(),
-                               'optim_dict' : optimizer.state_dict()},
+                               'state_dict': model.state_dict(),
+                               'optim_dict': optimizer.state_dict()},
                                is_best=is_best,
                                checkpoint=model_path)
 
         # If best_eval, best_save_path
         if is_best:
-            logging.info("- Found new best accuracy")
+            logging.info("- Found new best F1 score!")
             best_val_acc = val_acc
 
             # Save best val metrics in a json file in the model directory
@@ -132,9 +134,9 @@ def train_and_evaluate(model, train_data, val_data, epochs, batch_size, optimize
         last_json_path = os.path.join(model_path, EXPERIMENT_PREFIX + "metrics_val_last_weights.json")
         utils.save_dict_to_json(val_metrics, last_json_path)
 
-        
-def roc_confusion(model, data, mode='Train', batch_size=100):
-    batch_outputs = [model(torch.from_numpy(data['data'][i: i + batch_size]).float()).detach().numpy()\
+
+def roc_confusion(model, data, mode='Train', batch_size=BATCH_SIZE):
+    batch_outputs = [model(torch.from_numpy(data['data'][i: i + batch_size]).float()).detach().numpy()
                                                                 for i in range(0, len(data['data']), batch_size)]
     '''batch_outputs = []
     for i in range(0, len(data['data']), batch_size):
@@ -144,22 +146,19 @@ def roc_confusion(model, data, mode='Train', batch_size=100):
     confusion = confusion_matrix(data['labels'], output)
     roc_auc_score = roc(data['labels'], output)
     logging.info('- {} ROC AUC score: {}'.format(mode, roc_auc_score))
-    print('{} confusion matrix:\n'.format(mode), confusion)
+    logging.info('{} confusion matrix:'.format(mode), confusion)
+    logging.info(str(confusion[0]))
+    logging.info(str(confusion[1]))
 
 
 if __name__ == '__main__':
-    # Load the parameters from json file
-    #args = parser.parse_args()
-    #json_path = os.path.join(model_path, 'params.json')
-    #assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
-    # FROM CONSTANTS INSTEAD
-
     # use GPU if available
     cuda = torch.cuda.is_available()
 
     # Set the random seed for reproducible experiments
     torch.manual_seed(128)
-    if cuda: torch.cuda.manual_seed(128)
+    if cuda:
+        torch.cuda.manual_seed(128)
 
     # Set the logger
     utils.set_logger(os.path.join(MODEL_PATH, EXPERIMENT_PREFIX + 'train.log'))
@@ -169,24 +168,24 @@ if __name__ == '__main__':
 
     # load data
     data_loader = DataLoader(DATA_PATH,
-                             labels_path=None,
-                             categorical=False,
+                             labels_path=LABELS_PATH,
+                             categorical=CATEGORICAL,
                              max_length=MAX_LENGTH,
-                             header=None,
-                             ascii=False,
-                             russian=True,
-                             digits=True,
-                             punctuation=True,
-                             lower=False)
+                             header=HEADER,
+                             ascii=ASCII,
+                             russian=RUSSIAN,
+                             digits=DIGITS,
+                             punctuation=PUNCTUATION,
+                             lower=LOWER)
     train_data, val_data, train_labels, val_labels = data_loader.load_data(split=True)
     train_data = {'data': train_data, 'labels': train_labels}
     val_data = {'data': val_data, 'labels': val_labels}
 
-    logging.info("- done.")
+    logging.info("... done.")
 
     # Define the model and optimizer
     if cuda:
-        model = Crepe.cuda() # not finished yet
+        model = Crepe.cuda()  # not finished yet
     else:
         model = Crepe(vocabulary_size=data_loader.vocabulary_size,
                       channels=CHANNELS,
@@ -197,6 +196,8 @@ if __name__ == '__main__':
                       output_size=OUTPUT_SIZE)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    logging.info("Model compiled: {} parameters total.".format(sum([len(layer.view(-1)) for layer in model.parameters()])))
 
     # define the loss function
     loss_function = torch.nn.BCELoss()
